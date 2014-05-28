4e076d9b7d0eadf6cde4af1c5665907c1f87b7ca
Tom Schroeder <j.tom.schroeder@gmail.com>
2014-05-27 22:05:55 -0500

    Add spec require

--- containers/queue.cr
+++ containers/queue.cr
@@ -1,9 +1,10 @@
 
+require "spec"
 require "deque"
 
 class Queue(T)
   include Enumerable
-  
+
   def initialize
     @container = Deque(T).new
   end
@@ -13,11 +14,11 @@ class Queue(T)
     ary.each { |e| queue << e } unless ary.empty?
     queue
   end
-   
+
   def next
     @container.front
   end
-  
+
   def push(obj)
     @container.push_back(obj)
   end
@@ -29,15 +30,15 @@ class Queue(T)
   def pop
     @container.pop_front
   end
-  
+
   def size
     @container.size
   end
-  
+
   def empty?
     @container.empty?
   end
-  
+
   def each
     @container.each { |e| yield e }
   end
