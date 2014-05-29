65c830f46b9423e9a4fa506d0211f7199aeac39d
Tom Schroeder <j.tom.schroeder@gmail.com>
2014-05-29 08:43:11 -0500

    Use literal tuple with unpacking

--- containers/rb_tree_map_spec.cr
+++ containers/rb_tree_map_spec.cr
@@ -40,24 +40,23 @@ describe "RBTreeMap" do
   end
 
   describe "(non-empty)" do
-    make_named_tuple("TreeRandomArray", "tree", "random_array")
     def setup_tree()
       tree = RBTreeMap(Int32, Int32).new
       num_items = 1000
       random_array = Array.new(num_items) { rand(num_items) }
       random_array.each { |x| tree[x] = x * 2 }
-      return TreeRandomArray.new(tree, random_array)
+      {tree, random_array}
     end
 
     it "should return correct size (uniqify items first)" do
-      tuple = setup_tree(); tree, random_array = tuple.tree, tuple.random_array
+      tree, random_array = setup_tree()
 
       tree.empty?.should be_false
       tree.size.should eq(random_array.uniq!.size)
     end
 
     it "should return correct max and min" do
-      tuple = setup_tree(); tree, random_array = tuple.tree, tuple.random_array
+      tree, random_array = setup_tree()
 
       tree.min_key.should eq(random_array.min)
       tree.max_key.should eq(random_array.max)
@@ -66,19 +65,19 @@ describe "RBTreeMap" do
     end
 
     it "should not #has_key? keys it doesn't have" do
-      tuple = setup_tree(); tree = tuple.tree
+      tree, random_array = setup_tree()
 
       tree.has_key?(100000).should be_false
     end
 
     it "should #has_key? keys it does have" do
-      tuple = setup_tree(); tree, random_array = tuple.tree, tuple.random_array
+      tree, random_array = setup_tree()
 
       tree.has_key?(random_array[0]).should be_true
     end
 
     it "should remove all keys" do
-      tuple = setup_tree(); tree, random_array = tuple.tree, tuple.random_array
+      tree, random_array = setup_tree()
 
       random_array.uniq!.each do |key|
         tree.has_key?(key).should eq(true)
@@ -89,7 +88,7 @@ describe "RBTreeMap" do
     end
 
     it "should delete_min keys correctly" do
-      tuple = setup_tree(); tree, random_array = tuple.tree, tuple.random_array
+      tree, random_array = setup_tree()
 
       # random_array.uniq!.sort!.reverse! # TODO: figure out why this intermittently crashes (due to Array#reverse!)
       random_array = random_array.uniq!.sort!.reverse
@@ -102,7 +101,7 @@ describe "RBTreeMap" do
     end
 
     it "should delete_max keys correctly" do
-      tuple = setup_tree(); tree, random_array = tuple.tree, tuple.random_array
+      tree, random_array = setup_tree()
 
       random_array.uniq!.sort!
       until tree.empty?
@@ -114,7 +113,7 @@ describe "RBTreeMap" do
     end
 
     it "should let you iterate with #each" do
-      tuple = setup_tree(); tree, random_array = tuple.tree, tuple.random_array
+      tree, random_array = setup_tree()
 
       i = 0
       random_array.uniq!.sort!
