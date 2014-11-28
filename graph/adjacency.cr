f0221a0bae740d9c413ab3f445772a00125cd410
Tom Schroeder <j.tom.schroeder@gmail.com>
2014-11-28 14:12:47 -0600

    Refactored common graph implementations

--- graph/adjacency.cr
+++ graph/adjacency.cr
@@ -1,55 +1,7 @@
 
 require "./mutable"
 
-# TODO: refactor AdjancencyGraph's common implementation
-
-class DirectedAdjacencyGraph(T, EdgeList) < MutableGraph(T, DirectedEdge)
-
-  def initialize(*other_graphs)
-    @vertice_dict = {} of T => EdgeList(T)
-
-    other_graphs.each do |g|
-      g.each_vertex {|v| add_vertex v}
-      g.each_edge {|v,w| add_edge v,w}
-    end
-  end
-
-  def each_vertex
-    @vertice_dict.each_key { |k| yield k }
-  end
-
-  def each_adjacent(v)
-    begin
-      adjacency_list = @vertice_dict[v]
-      adjacency_list.each { |e| yield e }
-    rescue MissingKey
-      raise NoVertexError.new("No vertex #{v}.")
-    end
-  end
-
-  def has_vertex?(v)
-    @vertice_dict.has_key?(v)
-  end
-
-  def has_edge? (u, v)
-    has_vertex?(u) && @vertice_dict[u].includes?(v)
-  end
-
-  def add_vertex (v)
-    @vertice_dict[v] ||= EdgeList(T).new
-  end
-
-  def add_edge(u, v)
-    add_vertex(u) # ensure key
-    add_vertex(v) # ensure key
-    basic_add_edge(u, v)
-  end
-
-  def remove_vertex (v)
-    @vertice_dict.delete(v)
-    # remove v from all adjacency lists
-    @vertice_dict.each_value { |adjList| adjList.delete(v) }
-  end
+class DirectedAdjacencyGraph(T, EdgeList) < MutableGraph(T, DirectedEdge, EdgeList)
 
   def directed?; true; end
 
@@ -63,55 +15,7 @@ class DirectedAdjacencyGraph(T, EdgeList) < MutableGraph(T, DirectedEdge)
 
 end
 
-class AdjacencyGraph(T, EdgeList) < MutableGraph(T, UndirectedEdge)
-
-  def initialize(*other_graphs)
-    @vertice_dict = {} of T => EdgeList(T)
-    
-    other_graphs.each do |g|
-      g.each_vertex {|v| add_vertex v}
-      g.each_edge {|v,w| add_edge v,w}
-    end
-  end
-
-  def each_vertex
-    @vertice_dict.each_key { |k| yield k }
-  end
-
-  def each_adjacent(v)
-    begin
-      adjacency_list = @vertice_dict[v]
-      adjacency_list.each { |e| yield e }
-    rescue MissingKey
-      raise NoVertexError.new("No vertex #{v}.")
-    end
-  end
-
-  def has_vertex?(v)
-    @vertice_dict.has_key?(v)
-  end
-
-  def has_edge? (u, v)
-    has_vertex?(u) && @vertice_dict[u].includes?(v)
-  end
-
-  def add_vertex (v)
-    @vertice_dict[v] ||= EdgeList(T).new
-  end
-
-  def add_edge(u, v)
-    add_vertex(u) # ensure key
-    add_vertex(v) # ensure key
-    basic_add_edge(u, v)
-  end
-
-  def remove_vertex (v)
-    @vertice_dict.delete(v)
-    # remove v from all adjacency lists
-    @vertice_dict.each_value { |adjList| adjList.delete(v) }
-  end
-
-  # above methods identical to DirectedAdjacencyGraph
+class AdjacencyGraph(T, EdgeList) < MutableGraph(T, UndirectedEdge, EdgeList)
 
   def directed?; false; end
 
--- graph/mutable.cr
+++ graph/mutable.cr
@@ -1,10 +1,33 @@
 
 require "./base"
 
-abstract class MutableGraph(T, Edge) < Graph(T, Edge)
+abstract class MutableGraph(T, Edge, EdgeList) < Graph(T, Edge)
 
-  abstract def add_vertex(v)
-  abstract def add_edge(u, v)
+  def initialize(*other_graphs)
+    @vertice_dict = {} of T => EdgeList(T)
+    
+    other_graphs.each do |g|
+      g.each_vertex {|v| add_vertex v}
+      g.each_edge {|v,w| add_edge v,w}
+    end
+  end
+
+  def each_vertex
+    @vertice_dict.each_key { |k| yield k }
+  end
+
+  def has_vertex?(v)
+    @vertice_dict.has_key?(v)
+  end
+
+  def each_adjacent(v)
+    begin
+      adjacency_list = @vertice_dict[v]
+      adjacency_list.each { |e| yield e }
+    rescue MissingKey
+      raise NoVertexError.new("No vertex #{v}.")
+    end
+  end
 
   def add_vertices(a)
     a.each { |v| add_vertex v }
@@ -14,11 +37,32 @@ abstract class MutableGraph(T, Edge) < Graph(T, Edge)
     edges.each { |edge| add_edge(edge[0], edge[1]) }
   end
 
-  abstract def remove_vertex(v)
   abstract def remove_edge(u, v)
-  
+
   def remove_vertices(a)
     a.each { |v| remove_vertex v }
+  end
+
+  def has_edge? (u, v)
+    has_vertex?(u) && @vertice_dict[u].includes?(v)
+  end
+
+  def add_vertex (v)
+    @vertice_dict[v] ||= EdgeList(T).new
+  end
+
+  protected abstract def basic_add_edge(u, v)
+
+  def add_edge(u, v)
+    add_vertex(u) # ensure key
+    add_vertex(v) # ensure key
+    basic_add_edge(u, v)
+  end
+
+  def remove_vertex (v)
+    @vertice_dict.delete(v)
+    # remove v from all adjacency lists
+    @vertice_dict.each_value { |adjList| adjList.delete(v) }
   end
 
 end
