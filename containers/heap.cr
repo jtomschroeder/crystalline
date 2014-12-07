585ff21ac7828ffb6375ecba3c2422932568f490
Tom Schroeder <j.tom.schroeder@gmail.com>
2014-12-06 19:23:59 -0600

    Implemented Fibonacci Heap with spec

---
+++ containers/heap.cr
@@ -0,0 +1,350 @@
+
+class Heap(T)
+  # include Enumerable(T)
+
+  def size
+    @size
+  end
+  alias_method :length, :size
+  
+  def initialize(ary = [] of T, @comparator = ->(x : T, y : T) { (x <=> y) == -1 })
+    @next = nil
+    @size = 0
+    @stored = {} of T? => Array(Node(T))
+    
+    ary.each { |n| push(n) } unless ary.empty?
+  end
+  
+  def push(key : T, value = key)
+    node = Node.new(key, value)
+    # Add new node to the left of the @next node
+    if nxt = @next
+      node.right = nxt
+      node.left = nxt.left
+      node.left.not_nil!.right = node
+      nxt.left = node
+      if nxt.key.nil? || @comparator.call(key, nxt.key.not_nil!)
+        @next = node
+      end
+    else
+      @next = node
+    end
+    @size += 1
+    
+    arr = [] of T
+    if nxt = @next
+      w = nxt.right
+      until w == @next
+        arr << w.not_nil!.value
+        w = w.not_nil!.right
+      end
+      arr << nxt.value
+    end
+
+    @stored[key] ||= [] of Node(T)
+    @stored[key] << node
+    value
+  end
+  alias_method :<<, :push
+  
+  def has_key?(key)
+    @stored[key] && !@stored[key].empty?
+  end
+  
+  def next
+    @next.try &.value
+  end
+  
+  def next_key
+    @next.try &.key
+  end
+  
+  def clear
+    @next = nil
+    @size = 0
+    @stored = {} of T? => Array(Node(T))
+    nil
+  end
+  
+  def empty?
+    @next.nil?
+  end
+
+  protected getter stored
+  protected def next_node
+    @next
+  end
+
+  def merge!(otherheap : Heap(T))
+    if other_root = otherheap.next_node
+      
+      # merge @stored hash's
+      otherheap.stored.each do |key, value|
+        if @stored.has_key? key
+          @stored[key] += value
+        else
+          @stored[key] = value
+        end
+      end
+
+      # Insert othernode's @next node to the left of current @next
+      if nxt = @next
+        nxt.left.not_nil!.right = other_root
+        if ol = other_root.left
+          other_root.left = nxt.left
+          ol.right = nxt
+          nxt.left = ol
+        end
+
+        @next = other_root if other_root.key.nil? || nxt.key.nil? || @comparator.call(other_root.key.not_nil!, nxt.key.not_nil!)
+      end
+    end
+    @size += otherheap.size
+  end
+  
+  def pop
+    if popped = @next
+      if @size == 1
+        clear
+        return popped.value
+      end
+    else
+      return
+    end
+
+    # Merge the popped's children into root node
+    if nxt = @next
+      if child = nxt.child
+        child.parent = nil
+        
+        # get rid of parent
+        sibling = child.right
+        until sibling == nxt.child
+          sibling.not_nil!.parent = nil
+          sibling = sibling.not_nil!.right
+        end
+        
+        # Merge the children into the root. If @next is the only root node, make its child the @next node
+        if nxt.right == nxt
+          @next = nxt.child
+        else
+          next_left, next_right = nxt.left, nxt.right
+          if current_child = nxt.child
+            nxt.right.not_nil!.left = current_child
+            nxt.left.not_nil!.right = current_child.right
+            current_child.right.not_nil!.left = next_left
+            current_child.right = next_right
+          end
+          @next = nxt.right
+        end
+      else
+        nxt.left.not_nil!.right = nxt.right
+        nxt.right.not_nil!.left = nxt.left
+        @next = nxt.right
+      end
+    end
+    consolidate
+    
+    unless @stored[popped.key].delete(popped)
+      raise "Couldn't delete node from stored nodes hash" 
+    end
+    @size -= 1
+    
+    popped.value
+  end
+  alias_method :next!, :pop
+
+  def change_key(key : T, new_key : T)
+    return if @stored[key].nil? || @stored[key].empty? || (key == new_key)
+    
+    # Must maintain heap property
+    raise "Changing this key would not maintain heap property!" unless @comparator.call(new_key, key)
+    if node = @stored[key].shift
+      node.key = new_key
+      @stored[new_key] ||= [] of Node(T)
+      @stored[new_key] << node
+      if parent = node.parent
+        # if heap property is violated
+        if parent.key.nil? || @comparator.call(new_key, parent.key.not_nil!)
+          cut(node, parent)
+          cascading_cut(parent)
+        end
+      end
+      if nxt = @next
+        if node.key.nil? || nxt.key.nil? || @comparator.call(node.key.not_nil!, nxt.key.not_nil!)
+          @next = node
+        end
+      end
+      return [node.key, node.value]
+    end
+    nil
+  end
+
+  def delete_key(key)
+    return if !@stored.has_key?(key) || @stored[key].empty?
+    
+    if node = @stored[key].shift
+      node.key = nil
+      @stored[nil] = [node]
+      parent = node.parent
+      if parent
+        cut(node, parent)
+        cascading_cut(parent)
+      end
+      @next = node
+      return [nil, node.value]
+    end
+    nil
+  end
+
+  def delete(key)
+    pop if delete_key(key)
+  end
+  
+  # private
+  class Node(T)
+    property :parent, :child, :left, :right, :key, :value, :degree, :marked
+
+    def initialize(@key : T, @value : T)
+      @degree = 0
+      @marked = false
+      @right :: Node(T)?
+      @right = self
+      @left :: Node(T)?
+      @left = self
+    end
+    
+    def marked?
+      @marked == true
+    end
+    
+  end
+  
+  # make node a child of a parent node
+  private def link_nodes(child, parent)
+    # link the child's siblings
+    child.left.not_nil!.right = child.right
+    child.right.not_nil!.left = child.left
+
+    child.parent = parent
+    
+    # if parent doesn't have children, make new child its only child
+    if parent.child.nil?
+      parent.child = child.right = child.left = child
+    else # otherwise insert new child into parent's children list
+      if current_child = parent.child
+        child.left = current_child
+        child.right = current_child.right
+        current_child.right.not_nil!.left = child
+        current_child.right = child
+      end
+    end
+    parent.degree += 1
+    child.marked = false
+  end
+  
+  # Makes sure the structure does not contain nodes in the root list with equal degrees
+  private def consolidate
+    roots = [] of Node(T)
+    root = @next
+    min = root
+    # find the nodes in the list
+    loop do
+      if r = root
+        roots << r
+        root = r.right
+      end
+      break if root == @next
+    end
+    degrees = {} of T => Node(T)
+    roots.each do |rt|
+      if m = min
+        if key = rt.key
+          min = rt if m.key.nil? || @comparator.call(key, m.key.not_nil!)
+        end
+      end
+      # check if we need to merge
+      unless degrees.has_key? rt.degree
+        degrees[rt.degree] = rt
+        next
+      else # there is another node with the same degree, consolidate them
+        degree = rt.degree
+        while degrees.has_key? degree
+          other_root_with_degree = degrees[degree]
+          if rt.key.nil? || other_root_with_degree.key.nil? || @comparator.call(rt.key.not_nil!, other_root_with_degree.key.not_nil!) # determine which node is the parent, which one is the child
+            smaller, larger = rt, other_root_with_degree
+          else
+            smaller, larger = other_root_with_degree, rt
+          end
+          link_nodes(larger, smaller)
+          degrees.delete(degree)
+          rt = smaller
+          degree += 1
+        end
+        degrees[degree] = rt
+        min = rt if min.not_nil!.key == rt.key # this fixes a bug with duplicate keys not being in the right order
+      end
+    end
+    @next = min
+  end
+  
+  private def cascading_cut(node)
+    p = node.parent
+    if p
+      if node.marked?
+        cut(node, p)
+        cascading_cut(p)
+      else
+        node.marked = true
+      end
+    end
+  end
+  
+  # remove x from y's children and add x to the root list
+  private def cut(x : Node(T), y : Node(T))
+    x.left.not_nil!.right = x.right
+    x.right.not_nil!.left = x.left
+    y.degree -= 1
+    if (y.degree == 0)
+      y.child = nil
+    elsif (y.child == x)
+      y.child = x.right
+    end
+    x.right = @next
+    x.left = @next.not_nil!.left
+    @next.not_nil!.left = x
+    x.left.not_nil!.right = x
+    x.parent = nil
+    x.marked = false
+  end
+  
+end
+
+
+class MaxHeap(T) < Heap(T)
+  def initialize(ary = [] of T)
+    super(ary, ->(x : T, y : T) { (x <=> y) == 1 })
+  end
+  
+  def max
+    self.next
+  end
+
+  def max!
+    self.pop
+  end
+end
+
+class MinHeap(T) < Heap(T)
+  def initialize(ary = [] of T)
+    super(ary, ->(x : T, y : T) { (x <=> y) == -1 })
+  end
+  
+  def min
+    self.next
+  end
+
+  def min!
+    self.pop
+  end
+end
---
+++ spec/heap_spec.cr
@@ -0,0 +1,162 @@
+
+require "../containers/heap"
+require "spec"
+
+def setup
+  random_array = [] of Int32
+  num_items = 100
+  num_items.times { random_array << rand(num_items) }
+  heap = MaxHeap(Int32).new(random_array)
+  {heap, random_array, num_items}
+end
+
+describe "Heap" do
+  
+  describe "(empty)" do
+
+    it "should return nil when getting the maximum" do
+      heap = MaxHeap(Int32).new
+      heap.max!.should be_nil
+    end
+    
+    it "should let you insert and remove one item" do
+      heap = MaxHeap(Int32).new
+
+      heap.size.should eq(0)
+      
+      heap.push(1)
+      heap.size.should eq(1)
+      
+      heap.max!.should eq(1)
+      heap.size.should eq(0)
+    end
+    
+    it "should let you initialize with an array" do
+      heap = MaxHeap(Int32).new([1, 2, 3])
+      heap.size.should eq(3)
+    end
+
+  end
+  
+  describe "(non-empty)" do
+    
+    it "should display the correct size" do
+      heap, random_array, num_items = setup()
+      heap.size.should eq(num_items)
+    end
+    
+    it "should have a next value" do
+      heap, random_array, num_items = setup()
+      heap.next.should be_truthy
+      heap.next_key.should be_truthy
+    end
+    
+    it "should delete random keys" do
+      heap, random_array, num_items = setup()
+      heap.delete(random_array[0]).should eq(random_array[0])
+      heap.delete(random_array[1]).should eq(random_array[1])
+      
+      ordered = [] of Int32
+      until heap.empty?
+        if i = heap.max!
+          ordered << i
+        end
+      end
+      ordered.should eq random_array[2..-1].sort.reverse
+    end
+    
+    it "should delete all keys" do
+      heap, random_array, num_items = setup()
+      ordered = [] of Int32
+      random_array.size.times do |t|
+        if i = heap.delete(random_array[t])
+          ordered << i
+        end
+      end
+      heap.empty?.should be_true
+      ordered.should eq random_array
+    end
+
+    it "should be in max->min order" do
+      heap, random_array, num_items = setup()
+      ordered = [] of Int32
+      until heap.empty?
+        if i = heap.max!
+          ordered << i
+        end
+      end
+      ordered.should eq random_array.sort.reverse
+    end
+    
+    it "should change certain keys" do
+      numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 101]
+      heap = MinHeap(Int32).new(numbers)
+      heap.change_key(101, 50)
+      heap.pop
+      heap.pop
+      heap.change_key(8, 0)
+      ordered = [] of Int32
+      until heap.empty?
+        if i = heap.min!
+          ordered << i
+        end
+      end
+      ordered.should eq [8, 3, 4, 5, 6, 7, 9, 10, 101, 100]
+    end
+    
+    it "should not delete keys it doesn't have" do
+      ary = [1, 2, 3, 4, 5, 6, 7, 8]
+      heap = MaxHeap(Int32).new(ary)
+      heap.delete(0).should be_nil
+      heap.size.should eq ary.length
+    end
+    
+    it "should delete certain keys" do
+      numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 101]
+      heap = MinHeap(Int32).new(numbers)
+      heap.delete(5)
+      heap.pop
+      heap.pop
+      heap.delete(100)
+      ordered = [] of Int32
+      until heap.empty?
+        if i = heap.min!
+          ordered << i
+        end
+      end
+      ordered.should eq [3, 4, 6, 7, 8, 9, 10, 101]
+    end
+    
+    it "should let you merge with another heap" do
+      heap, random_array, num_items = setup()
+      numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+      otherheap = MaxHeap(Int32).new(numbers)
+      otherheap.size.should eq(8)
+      heap.merge!(otherheap)
+      
+      ordered = [] of Int32
+      until heap.empty?
+        if i = heap.max!
+          ordered << i
+        end
+      end
+      
+      ordered.should eq (random_array + numbers).sort.reverse
+    end
+    
+    describe "min-heap" do
+      it "should be in min->max order" do
+        heap, random_array, num_items = setup()
+        heap = MinHeap(Int32).new(random_array)
+        ordered = [] of Int32
+        until heap.empty?
+          if i = heap.min!
+            ordered << i
+          end
+        end
+        ordered.should eq random_array.sort
+      end
+    end
+    
+  end
+end
