1e15085d787977e3a0fae431979eb94f254b4ef9
Konovod <konovod@gmail.com>
2017-06-10 18:55:09 +0000

    stack specs passed

--- containers/stack.cr
+++ containers/stack.cr
@@ -1,14 +1,13 @@
-
 require "./deque"
 require "./common"
 
 class Stack(T)
-  include Enumerable
-  
+  include Enumerable(T)
+
   def initialize
     @container = Deque(T).new
   end
-  
+
   def self.new(ary : Array(T))
     stack = Stack(T).new
     ary.each { |e| stack << e } unless ary.empty?
@@ -16,21 +15,32 @@ class Stack(T)
   end
 
   def next
-    @container.back
-  end
-  
-  def push(obj)
-    @container.push_back(obj)
-  end
-  alias_method :<<, :push
-  
-  def pop
-    @container.pop_back as T?
+    @container.last
   end
 
-  delegate size, @container
-  delegate empty?, @container
-  
+  def next?
+    @container.last?
+  end
+
+  def push(obj)
+    @container.push(obj)
+  end
+
+  def <<(obj)
+    @container.push obj
+  end
+
+  def pop
+    @container.pop
+  end
+
+  def pop?
+    @container.pop?
+  end
+
+  delegate size, to: @container
+  delegate empty?, to: @container
+
   def each
     @container.reverse_each { |e| yield e }
   end
--- spec/stack_spec.cr
+++ spec/stack_spec.cr
@@ -1,12 +1,11 @@
-
 require "spec"
 require "../containers/stack"
 
 describe "Stack" do
   describe "(empty)" do
-    it "should return nil when sent #pop" do
+    it "should return nil when sent #pop?" do
       stack = Stack(Int32).new
-      stack.pop.should be_nil
+      stack.pop?.should be_nil
     end
 
     it "should return a size of 1 when sent #push" do
@@ -15,9 +14,9 @@ describe "Stack" do
       stack.size.should eq(1)
     end
 
-    it "should return nil when sent #next" do
+    it "should return nil when sent #next?" do
       stack = Stack(Int32).new
-      stack.next.should be_nil
+      stack.next?.should be_nil
     end
 
     it "should return empty?" do
@@ -42,7 +41,6 @@ describe "Stack" do
       stack.empty?.should be_false
     end
 
-
     it "should iterate in LIFO order" do
       stack = Stack(Int32).new([1, 2, 3, 5, 8])
       arr = [] of Int32
@@ -53,8 +51,8 @@ describe "Stack" do
     it "should return nil after all pops" do
       stack = Stack(Int32).new([1, 2, 3, 5, 8])
       5.times { stack.pop }
-      stack.pop.should be_nil
-      stack.next.should be_nil
+      stack.pop?.should be_nil
+      stack.next?.should be_nil
     end
   end
 end
