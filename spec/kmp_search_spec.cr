6f2155f320e2c1a40abbb5dae4e32a0329fa1cee
Simon <fake@e.mail>
2018-03-01 02:17:39 -0500

    add spec

--- spec/kmp_search_spec.cr
+++ spec/kmp_search_spec.cr
@@ -2,10 +2,15 @@ require "spec"
 require "../algorithms/kmp_search"
 
 describe "KMP Search" do
-  describe "find some pattern" do
-    it "returns some indices in the given text" do
+  describe "finds some pattern" do
+    it "returns its indices in the given text" do
       value = Algorithms::Search.kmp_search("babccabc", "abc")
       value.should eq([1, 5])
+    end
+
+    it "returns no results" do
+      value = Algorithms::Search.kmp_search("the quick brown fox jumped over the lazy dog.", "abc")
+      value.should eq([] of Int32)
     end
   end
 end
