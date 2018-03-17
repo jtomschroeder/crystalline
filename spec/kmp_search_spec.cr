0e9219b02836b6d6f8b6f01e28f60974e0241deb
spaill <paillard.simon7@gmail.com>
2018-03-17 14:13:18 -0400

    Kmp (#11)
    * implement kmp search

---
+++ spec/kmp_search_spec.cr
@@ -0,0 +1,16 @@
+require "spec"
+require "../algorithms/kmp_search"
+
+describe "KMP Search" do
+  describe "finds some pattern" do
+    it "returns its indices in the given text" do
+      value = Algorithms::Search.kmp_search("babccabc", "abc")
+      value.should eq([1, 5])
+    end
+
+    it "returns no results" do
+      value = Algorithms::Search.kmp_search("the quick brown fox jumped over the lazy dog.", "abc")
+      value.should eq([] of Int32)
+    end
+  end
+end
